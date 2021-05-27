16d1ff8e6c561fecfaba7d7cf1989aaa905deec4
williamstewart <wrcstewart@yahoo.co.uk>
2021-05-27 14:38:45 +0100

    nothing much changed?

--- index.js
+++ index.js
@@ -15,12 +15,17 @@ const io = require('socket.io')(server);
 
 app.get('/', (req,res)=>{
 
-    res.sendFile(path.resolve(__dirname,'index1.html'))
+    res.sendFile(path.resolve(__dirname,'index3.html'))
 
 })
 
+let usersObj = {};
+let testUser = "user1";
+let chatPairs ={};
+console.log(usersObj[testUser]);
 
-io.on('connection', (socket)=>{console.log("sock it")});
+io.on('connection', (socket)=>{console.log("socketID "+socket.id)});
+//io.on('connection', (socket)=>{console.log( socket.id    )});
 /*
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
@@ -30,7 +35,47 @@ io.on('connection', (socket) => {
 */
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-        console.log(msg)
+
+
+
+        io.to(socket.id).emit('chat message',{user:"self",message:msg});
+       io.to(chatPairs[socket.id]).emit('chat message',{user:"buddy", message:msg});
+        //io.emit('chat message', msg);
+        //console.log(msg);
+        //console.log(socket.id);
     });
 });
+
+io.on('connection', (socket) => {
+    socket.on('client data', (msg) => {
+        //io.emit('chat message', msg);
+        console.log(msg);
+        addUserToUserObj(msg);
+        /*
+       io.to(msg.socketID).emit('chat message',"private message from yourself");
+        io.to(chatPairs[msg.socketID]).emit('chat message',"private message from chat buddy");
+        */
+
+        //console.log(socket.id);
+    });
+});
+
+function addUserToUserObj(msg){
+
+usersObj[msg.user] = msg.socketID;
+
+console.log(usersObj);
+
+if(msg.user == "user2" ) testInitialiseChatPairs();
+
+}
+
+function testInitialiseChatPairs(){
+// assumes as an example user1 and user 2 have agreed a chat.
+
+chatPairs[usersObj["user1"]] = usersObj["user2"];//should be socket ids
+chatPairs[usersObj["user2"]] = usersObj["user1"];
+console.log(chatPairs)
+
+}
+
