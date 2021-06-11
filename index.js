6e6e8aeeb66e93f70800e37a6b39a667518f60b2
williamstewart <wrcstewart@yahoo.co.uk>
2021-06-11 07:44:48 +0100

    databaseaccess basic

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+
+//mongoose.connect(' mongodb+srv://wrcstewart:Ginger>1951@cluster0.ewegv.mongodb.net/my_database', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 const express = require('express');
 const path = require('path');
+
 
 const app = express();
 
@@ -10,19 +13,63 @@ if (port == null || port == "") {
 const server = app.listen(port, ()=>{
     console.log('App listening on 4000')
 })
+const url = 'mongodb+srv://wrcstewart:Ginger>1951@cluster0.ewegv.mongodb.net/my_database';
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+
+const assert = require('assert');
+const dbName = 'my_database2';
+const client = new MongoClient(url, {useNewUrlParser: true});
+
+client.connect(function(err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    db.collection('Petals').insertOne({
+
+     title:'Better Post!',
+
+     slug:'a-better-post',
+
+     published: true,
+
+     author: 'Ado Kukic',
+
+     content: 'This is an even better post',
+
+     tags: ['featured'],
+
+})
+
+
+
+
+});
+
+
+
+
+
+
+
 
 const io = require('socket.io')(server);
 
 app.get('/', (req,res)=>{
 
-    res.sendFile(path.resolve(__dirname,'index3.html'))
+    res.sendFile(path.resolve(__dirname,'index5.html'))
 
 })
 
 let usersObj = {};
+let invUsersObj = {};
 let testUser = "user1";
 let chatPairs ={};
 console.log(usersObj[testUser]);
+getCompactDateTime();
 
 io.on('connection', (socket)=>{console.log("socketID "+socket.id)});
 //io.on('connection', (socket)=>{console.log( socket.id    )});
@@ -46,6 +93,33 @@ io.on('connection', (socket) => {
         //console.log(msg);
         //console.log(socket.id);
     });
+    socket.on('publish', (msg) => {
+
+
+
+
+
+
+        // do all the database stuff here
+        msg = constructInfoBar(socket.id) + "<br>" + msg;
+
+
+        let status = 'publish';
+       // console.log(status);
+
+        io.to(socket.id).emit('chat message',{user:"self",status:status,message:msg});
+       io.to(chatPairs[socket.id]).emit('chat message',{user:"buddy",status:status, message:msg});
+        //io.emit('chat message', msg);
+        //console.log(msg);
+        //console.log(socket.id);
+        console.log(constructInfoBar(socket.id))
+
+
+
+    });
+
+
+
     socket.on('help', (msg) => {
         let helpMessage ="";
         if( msg === 'explain panes'){
@@ -73,13 +147,15 @@ io.on('connection', (socket) => {
 
         //console.log(socket.id);
     });
-});
+});//client data
 
 function addUserToUserObj(msg){
 
 usersObj[msg.user] = msg.socketID;
+invUsersObj[msg.socketID] = msg.user;
 
 console.log(usersObj);
+console.log(invUsersObj);
 
 if(msg.user == "user2" ) testInitialiseChatPairs();
 
@@ -92,5 +168,76 @@ chatPairs[usersObj["user1"]] = usersObj["user2"];//should be socket ids
 chatPairs[usersObj["user2"]] = usersObj["user1"];
 console.log(chatPairs)
 
+}
+
+function getReadableDateTime(){
+    let date_ob = new Date();
+
+// current date
+// adjust 0 before single digit date
+let date = ("0" + date_ob.getUTCDate()).slice(-2);
+
+// current month
+let month = ("0" + (date_ob.getUTCMonth() + 1)).slice(-2);
+
+// current year
+let year = date_ob.getUTCFullYear();
+
+// current hours
+let hours = date_ob.getUTCHours();
+
+// current minutes
+let minutes = date_ob.getUTCMinutes();
+
+// current seconds
+let seconds = date_ob.getUTCSeconds();
+
+// prints date in YYYY-MM-DD format
+//console.log(year + "-" + month + "-" + date);
+
+// prints date & time in YYYY-MM-DD HH:MM:SS format
+return(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds +" UTC");
+
+}
+function getCompactDateTime(){
+    let date_ob = new Date();
+
+// current date
+// adjust 0 before single digit date
+let date = ("0" + date_ob.getUTCDate()).slice(-2);
+
+// current month
+let month = ("0" + (date_ob.getUTCMonth() + 1)).slice(-2);
+
+// current year
+let year = date_ob.getUTCFullYear();
+
+// current hours
+let hours = date_ob.getUTCHours();
+
+// current minutes
+let minutes = date_ob.getUTCMinutes();
+
+// current seconds
+let seconds = date_ob.getUTCSeconds();
+
+// prints date in YYYY-MM-DD format
+//console.log(year + "-" + month + "-" + date);
+
+// prints date & time in YYYY-MM-DD HH:MM:SS format
+return("_"+year +"_" + month +"_" + date + "___" + hours + "_" + minutes + "_" + seconds +"_UTC");
+
+}
+
+function constructInfoBar(sockId){
+    let outString;
+    let readableDate = getReadableDateTime();
+    let userSelf = invUsersObj[sockId]
+    //console.log(userSelf)
+    let clientId =chatPairs[sockId];
+    let userBuddy = invUsersObj[clientId];
+    //console.log(userBuddy);
+    let outName = userBuddy + "_" + userSelf;
+    return(outName + getCompactDateTime());
 }
 
